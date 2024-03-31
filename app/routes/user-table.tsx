66849522e5792d9d68d66280bb4dd8e8f0c5bcd3
Peng Sha <me@pengsha.dev>
2024-03-31 23:29:15 +0800

    refactor: reduce code length

--- app/routes/user-table.tsx
+++ app/routes/user-table.tsx
@@ -2,20 +2,36 @@ import { useEffect } from 'react';
 import { useSearchParams } from '@remix-run/react';
 import { UserTable } from '~/components/user-table';
 import {
-  initSearchParamsFromDefaultUserFilter,
-  getUserFilterFromSearchParams,
-} from '~/components/user-table/user-table.utils';
+  defaultPage,
+  defaultLimit,
+} from '~/components/user-table/user-table.store';
+import type { UserStatus } from '~/components/user-table/user-table.constants';
 
 const UserTablePage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
-    setSearchParams(initSearchParamsFromDefaultUserFilter);
+    setSearchParams((prev) => {
+      if (!prev.get('page')) {
+        prev.set('page', String(defaultPage));
+      }
+      if (!prev.get('limit')) {
+        prev.set('limit', String(defaultLimit));
+      }
+      return prev;
+    });
   }, [setSearchParams]);
 
   return (
     <div className="container mx-auto py-10">
-      <UserTable userFilter={getUserFilterFromSearchParams(searchParams)} />
+      <UserTable
+        userFilter={{
+          limit: Number(searchParams.get('limit') ?? defaultLimit),
+          page: Number(searchParams.get('page') ?? defaultPage),
+          name: searchParams.get('name') ?? undefined,
+          status: (searchParams.get('status') as UserStatus) ?? undefined,
+        }}
+      />
     </div>
   );
 };
